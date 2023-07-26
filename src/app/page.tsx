1681e86d8049ceb16227561c76e53220867db785
Aurelio Borges <aurelioaborges@gmail.com>
2023-07-26 12:29:03 -0300

    config: initial SASS setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image'
-import styles from './page.module.css'
+import styles from './page.module.scss'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
+          LA Mountains&nbsp;
+          <code className={styles.code}>Landing Page</code>
         </p>
         <div>
           <a
